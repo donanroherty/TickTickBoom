1f02e4f0a2a2975b0f0fb0fe57abbe6f9cabea8c
Ronan Doherty <donanroherty@gmail.com>
2018-01-13 22:17:47 +0000

    Removed unecessary includes from game mode

--- Game/Source/TickTickBoom/Private/TTBGameMode.cpp
+++ Game/Source/TickTickBoom/Private/TTBGameMode.cpp
@@ -1,16 +1,6 @@
 
 
 #include "TTBGameMode.h"
-#include "Runtime/CoreUObject/Public/UObject/ConstructorHelpers.h"
-#include "Engine/World.h"
-#include "TimerManager.h"
-#include "Runtime/Engine/Public/EngineGlobals.h"
-#include "Engine/DataTable.h"
-#include "TTBGameBoard.h"
-#include "TTBHud.h"
-#include "TTBPlayerController.h"
-#include "TTBGameInstance.h"
-#include "TTBGameState.h"
 
 ATTBGameMode::ATTBGameMode()
 {
